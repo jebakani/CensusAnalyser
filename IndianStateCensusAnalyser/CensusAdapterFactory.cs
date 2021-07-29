89426e62769e587494397bf4ce15cd446cac8590
Jebakani <vjishwarya@gmail.com>
2021-07-29 19:09:07 +0530

    [JEBAKANI] Creating the census analyser to check for indian state census information

---
+++ IndianStateCensusAnalyser/CensusAdapterFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    class CensusAdapterFactory
+    {
+        public Dictionary<string,CensusData> LoadCsvData(CensusAnalyser.Country country,string csvFilePath,string dataHeader)
+        {
+            switch(country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().
+            }
+        }
+    }
+}
---
+++ IndianStateCensusAnalyser/CensusData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    //class census data to get all posiible data
+    class CensusData
+    {
+        public int SrNo;
+        public string State;
+        public int TIN;
+        public string StateCode;
+        public string Population;
+        public string AreaInSqKm;
+        public string DensityPerSqKm;
+        public double totalArea;
+        public double waterLevel;
+
+        public CensusData(StateCodeDataDAO stateCodeData)
+        {
+            this.SrNo = stateCodeData.SrNo;
+            this.State = stateCodeData.State;
+            this.TIN = stateCodeData.TIN;
+            this.StateCode = stateCodeData.StateCode;
+        }
+
+        public CensusData(CensusDataDAO censusDataDAO)
+        {
+            this.State =censusDataDAO.State;
+            this.Population = censusDataDAO.Population;
+            this.AreaInSqKm = censusDataDAO.AreaInSqKm;
+            this.DensityPerSqKm = censusDataDAO.DensityPerSqKm;
+        }
+
+    }
+}
